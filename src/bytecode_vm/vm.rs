a9230b88cdd0af124df23e1deb390c79be09abb0
Raghav Bansal <raghavb1234@gmail.com>
2022-10-30 22:49:34 -0400

    Fix stack print for trace execution

--- src/bytecode_vm/vm.rs
+++ src/bytecode_vm/vm.rs
@@ -237,7 +237,7 @@ impl VM {
                 let ip = frame.ip;
                 let base_ptr = frame.base_ptr;
 
-                println!("STACK     {:?}", self.stack);
+                println!("STACK     {:?}", self.stack.stack);
                 println!(
                     "ip = {}, bp = {} ({:?})",
                     ip,
